a37188b46ac9fcad6aed793bca3e1ff0b2f457cb
Korvin <Dedok3d@github.com>
2020-03-22 20:56:10 +0700

    add aphrodite as CSS in TS library

--- src/form/form.tsx
+++ src/form/form.tsx
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
 
+const styles = StyleSheet.create({
+  container: {
+    border: '1px solid rgb(41, 72, 125)',
+    background: 'rgb(66, 103, 178)',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  logo: {
+    height: '24px',
+    margin: '4px',
+    paddingLeft: '5vh',
+    fontSize: 'larger',
+    cursor: 'pointer',
+    color: 'rgb(255,255,255)',
+  },
+});
 
-class Form extends Component<{}, {}> {
-
+class Form extends Component {
 
   render() {
 
     return (
-      <div>
-        <h1 >Hello World</h1>
+      <div className={css(styles.container)}>
+        <div className={css(styles.logo)}>ЦФТ</div>
       </div>
     );
   }
