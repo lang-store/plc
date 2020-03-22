a80a4750007fb81c8784f9e332d2768bb4e28055
Korvin <Dedok3d@github.com>
2020-03-22 21:36:18 +0700

    init selection component

---
+++ src/components/selection/selection.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+
+const styles = StyleSheet.create({
+  main: {
+  },
+});
+
+function Selection() {
+
+  return (
+    <div className={css(styles.main)}>
+      
+    </div>
+  );
+}
+
+export default Selection;
