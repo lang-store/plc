4620c7957ab7e9a475fa031ff3f377f3c49f2d95
Sergey <Sersh_96@mail.ru>
2020-04-03 00:13:54 +0700

    add simple spinner to Chart loader

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -14,6 +14,20 @@ import CompareRow from '../compare-row';
 import Java from '../../store/java.json';
 import Lisp from '../../store/lisp.json';
 
+const loadKeyframes = {
+  '0%': {
+    transform: 'rotate(0deg)',
+    'animation-timing-function': 'cubic-bezier(0.55, 0.055, 0.675, 0.19)',
+  },
+  '50%': {
+    transform: 'rotate(900deg)',
+    'animation-timing-function': 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+  },
+  '100%': {
+    transform: 'rotate(1800deg)',
+  }
+};
+
 const styles = StyleSheet.create({
   main: {
   },
@@ -45,6 +59,16 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  loader: {
+    width: '50px',
+    height: '50px',
+    border: '5px solid rgba(255,255,255,.3)',
+    borderRadius: '50%',
+    borderTopColor: 'rgb(66, 103, 178)',
+    animationName: [loadKeyframes],
+    animationDuration: '3s',
+    animationIterationCount: 'infinite',
   },
 });
 
@@ -94,7 +118,7 @@ function Main() {
                     width={'700px'}
                     height={'400px'}
                     chartType="Bar"
-                    loader={<div>Loading Chart</div>}
+                    loader={<div className={css(styles.loader)}></div>}
                     data={[
                       ['', languageA.name, languageB.name],
                       ...createData(key, [languageA, languageB])
