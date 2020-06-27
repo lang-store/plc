daa2ba0239ecff27f5c40defbd0160940265c394
Dedok3d <Sersh_96@mail.ru>
2020-06-27 17:50:21 +0700

    NF: add encapsulation of quotation marks on sql script

--- src/logic/sql/sql.ts
+++ src/logic/sql/sql.ts
@@ -4,13 +4,13 @@ class ScriptBuilder {
     generateSqlScript = (language: Language[]) => {
         let script = '';
         language.forEach(lang => {
-            script += `\n INSERT INTO languages (name) VALUES ('${lang.name}'); \n`;
+            script += `\n INSERT INTO languages (name) VALUES ('${lang.name.replace(/'/g, `''`)}'); \n`;
 
             lang.concepts.forEach(concept => {
-                script += `\n INSERT INTO concepts (name, category, method, language_id) VALUES ('${concept.name}', '${concept.category}', '${concept.method}', (select id from languages where name = '${lang.name}')); \n`;
+                script += `\n INSERT INTO concepts (name, category, method, language_id) VALUES ('${concept.name.replace(/'/g, `''`)}', '${concept.category}', '${concept.method}', (select id from languages where name = '${lang.name.replace(/'/g, `''`)}')); \n`;
 
                 concept.examples.forEach(example => {
-                    script += `\n INSERT INTO examples (example, notes, concept_id) VALUES ('${example.example}', '${example.notes}', (select id from concepts where name = '${concept.name}' and language_id = (select id from languages where name = '${lang.name}'))); \n`;
+                    script += `\n INSERT INTO examples (example, notes, concept_id) VALUES ('${example.example.replace(/'/g, `''`)}', '${example.notes.replace(/'/g, `''`)}', (select id from concepts where name = '${concept.name.replace(/'/g, `''`)}' and language_id = (select id from languages where name = '${lang.name}'))); \n`;
                 });
             });
         });
